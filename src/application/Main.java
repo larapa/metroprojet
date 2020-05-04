1c5726b3cfc5b09057636804525fbc7ea95f377a
larapa <57297451+larapa@users.noreply.github.com>
2020-05-04 20:30:39 +0200

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.VBox;
@@ -19,16 +20,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main extends Application  {
+	Label label;
 	Button bouton1;
 	Button bouton2;
 	Button bouton3;
+	Button bouton4;
 	Stage window;
 	Scene scene;
+	Scene scene2;
 	ComboBox<String> comboBox1;
 	ComboBox<String> comboBox2;
 	ComboBox<String> comboBox3;
 	ComboBox<String> comboBox4;
-	ListView<Integer> listview;
+	ListView<String> listview;
 	@Override
 	
 	public void start(Stage primaryStage) throws Exception {
@@ -93,7 +97,7 @@ public class Main extends Application  {
 		comboBox4.setPromptText("Choix du type d'itinéraire");
 		comboBox4.setEditable(true);
 		listview = new ListView<>();
-		listview.getItems().addAll(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
+		listview.getItems().addAll("1","2","3","3 bis","4","5","6","7","7 bis","8","9","10","11","12","13","14");
 		listview.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 		ArrayList<String> stationsinterdites = new ArrayList<String>();
 		bouton2 = new Button();
@@ -102,24 +106,40 @@ public class Main extends Application  {
 		bouton3 = new Button();
 		bouton3.setText("Supprimer");
 		bouton3.setOnAction(e ->  stationsinterdites.remove(comboBox3.getValue()) );
+		
+		
+		bouton4 = new Button();
+		bouton4.setText("Nouveau itinéraire");
+		bouton4.setOnAction(e -> window.setScene(scene)   );
+		
+		label = new Label();
+		label.setWrapText(true);
+		
+		//
 		bouton1 = new Button();
 		bouton1.setText("Calcul itinéraire");
+		
+		
+		
 		bouton1.setOnAction(e ->   {
 			try {
 				if (comboBox4.getValue().equals("Itinéraire avec le moins de changements")) {
-					MoinsDeChangements C = new MoinsDeChangements(comboBox1.getValue(), comboBox2.getValue(), tabint(),stationsinterdites);//important ici
-					System.out.println(C.afficheChemin2());
+					
+					PlusCourtChemin C = new PlusCourtChemin(comboBox1.getValue(), comboBox2.getValue(), tabint(),stationsinterdites,false);//important ici
+					label.setText(C.afficheChemin());
+					window.setScene(scene2);
 				}
 				else {
 					if(comboBox4.getValue().equals("Itinéraire le plus rapide")) {
-						PlusCourtChemin C = new PlusCourtChemin(comboBox1.getValue(), comboBox2.getValue(), tabint(),stationsinterdites);//important ici
-						System.out.println(C.afficheChemin()); //important ici
+						PlusCourtChemin C = new PlusCourtChemin(comboBox1.getValue(), comboBox2.getValue(), tabint(),stationsinterdites,true);//important ici
+						label.setText(C.afficheChemin()); //important ici
+						window.setScene(scene2);
 					}
 					else {
-						PlusCourtChemin C = new PlusCourtChemin(comboBox1.getValue(), comboBox2.getValue(), tabint(),stationsinterdites);//important ici
-						System.out.println(C.afficheChemin());
-						MoinsDeChangements CC = new MoinsDeChangements(comboBox1.getValue(), comboBox2.getValue(), tabint(),stationsinterdites);//important ici
-						System.out.println(CC.afficheChemin2()); 
+						PlusCourtChemin C = new PlusCourtChemin(comboBox1.getValue(), comboBox2.getValue(), tabint(),stationsinterdites,true);//important ici
+						PlusCourtChemin CC = new PlusCourtChemin(comboBox1.getValue(), comboBox2.getValue(), tabint(),stationsinterdites,false);//important ici
+						label.setText(C.afficheChemin() + "\n" +"\n" + CC.afficheChemin() );
+						window.setScene(scene2);
 					}
 				}
 			} 
@@ -127,18 +147,43 @@ public class Main extends Application  {
 				e1.printStackTrace();
 			}
 		});
+		
+		
+		
 		VBox layout = new VBox(10);
 		layout.setPadding(new Insets(20, 20, 20, 20));
 		layout.getChildren().addAll(comboBox1,comboBox2,listview,comboBox3,bouton2, bouton3,comboBox4, bouton1);
 		scene = new Scene(layout, 600, 500);
+		
+		
+		VBox layout2 = new VBox(10);
+		layout2.setPadding(new Insets(20, 20, 20, 20));		// 2eme scene
+		layout2.getChildren().addAll(label,bouton4);
+		scene2 = new Scene(layout2, 600, 500);
+		
+		
 		window.setScene(scene);
 		window.show();
+		
 		}
 		ArrayList<Integer> tabint() {
-			ObservableList<Integer> l;
+			ObservableList<String> l;
 			l = listview.getSelectionModel().getSelectedItems();
-			ArrayList<Integer> tab = new ArrayList<Integer>(l); // ObservableList<String> --> Arraylist<String>
-			return tab;
+			ArrayList<String> tab = new ArrayList<String>(l);// ObservableList<String> --> Arraylist<String>
+			ArrayList<Integer> tab2 = new ArrayList<Integer>();
+			for (String s: tab) {
+				if (s.equals("3 bis")) {
+					tab2.add(15);
+				}
+				else if (s.equals("7 bis")) {
+					tab2.add(16);
+				}
+				else {
+					tab2.add(Integer.parseInt(s));
+					
+				}
+			}
+			return tab2;
 		}
 
 		public static void main(String[] args) {
