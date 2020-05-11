a7cae7c04aa0af7279063894fe9db816309445d4
ShichengCZ <lscxidian@gmail.com>
2020-05-12 00:23:46 +0200

    Add files via upload

--- src/application/PlusCourtChemin.java
+++ src/application/PlusCourtChemin.java
@@ -1,8 +1,13 @@
-
 package application;
 
 
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 
 import java.io.File;
@@ -23,13 +28,15 @@ import java.util.Collections;
 
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
 
 
 public class PlusCourtChemin {
     boolean b;
-	String d√©part;
+	String d®¶part;
 
-	String arriv√©e;
+	String arriv®¶e;
 
 	ArrayList<Integer> LignesInterdites;
 
@@ -37,11 +44,11 @@ public class PlusCourtChemin {
 
 	
 
-	public PlusCourtChemin(String d√©part, String arriv√©e,ArrayList<Integer> LignesInterdites, ArrayList<String> StationsInterdites,boolean a) {
+	public PlusCourtChemin(String d®¶part, String arriv®¶e,ArrayList<Integer> LignesInterdites, ArrayList<String> StationsInterdites,boolean a) {
         this.b=a;
-		this.d√©part = d√©part;
+		this.d®¶part = d®¶part;
 
-		this.arriv√©e = arriv√©e;
+		this.arriv®¶e = arriv®¶e;
 
 		this.LignesInterdites = LignesInterdites;
 
@@ -63,17 +70,38 @@ public class PlusCourtChemin {
 
 
 
-		ArrayList<Arr√™te> arr√™tes = new  ArrayList<Arr√™te>();
+		ArrayList<Arr®∫te> arr®∫tes = new  ArrayList<Arr®∫te>();
 
-		List<String> arr√™tesString = null;
+		List<String> arr®∫tesString = null;
+
+		BufferedReader bf=new BufferedReader(new FileReader("./src/application/coordonnes1.txt"));
+		
+		String textLine;
+		
+		String str2 ="";
+		
+		while((textLine=bf.readLine())!=null){
+		
+			str2+=textLine;
+		}
+		
+		String[] numbers=str2.split("	");
+		
+		float []cord=new float[numbers.length]; 
+		
+		for (int i = 0; i < numbers.length; i++) {
+		
+			cord[i]=Float.parseFloat(numbers[i]);
+		}
+		
+		bf.close();
+
+		
+		arr®∫tesString = Files.readAllLines(Paths.get("./src/application/Arr®∫tes.txt"));
 
 
 
-		arr√™tesString = Files.readAllLines(Paths.get("./src/application/Arr√™tes.txt"));
-
-
-
-		for (String s: arr√™tesString) {
+		for (String s: arr®∫tesString) {
 
 			String str[] = s.split(" ");
 
@@ -87,7 +115,7 @@ public class PlusCourtChemin {
 
 			}
 
-			arr√™tes.add(new Arr√™te(b.get(0), b.get(1), b.get(2)+25));
+			arr®∫tes.add(new Arr®∫te(b.get(0), b.get(1), b.get(2)+25));
 
 		}
 
@@ -169,15 +197,17 @@ public class PlusCourtChemin {
 
 
 
-		//on cr√©e le graphe:
+		//on cr®¶e le graphe:
 
-		Graphe g = new Graphe(arr√™tes);
+		Graphe g = new Graphe(arr®∫tes);
 
 
 
 		g.donnenom(nom);
 
 		g.attribueligne(lignes);
+		
+		g.donnecord(cord);
 
 		//on prend en compte les contraintes
 
@@ -186,7 +216,7 @@ public class PlusCourtChemin {
 		g.retirerstation2(StationsInterdites);
 		if(!this.b) {
 
-		g.transformearr√™te();
+		g.transformearr®∫te();
 		}
 		int w=0;
 
@@ -200,18 +230,18 @@ public class PlusCourtChemin {
 
 
 
-		while (w<g.nombredeligne(d√©part)) {
+		while (w<g.nombredeligne(d®¶part)) {
 
 
 
 			int y=0;
 
-			while (y<g.nombredeligne(arriv√©e)) {
+			while (y<g.nombredeligne(arriv®¶e)) {
 
-				Graphe e = new Graphe(arr√™tes);
+				Graphe e = new Graphe(arr®∫tes);
 
 
-
+				
 				e.donnenom(nom);
 
 				e.attribueligne(lignes);
@@ -222,12 +252,12 @@ public class PlusCourtChemin {
 
 				if(!this.b) {
 
-					e.transformearr√™te();
+					e.transformearr®∫te();
 					}
 
-				e.calculpluscourtedistance(d√©part,arriv√©e,w,y);
+				e.calculpluscourtedistance(d®¶part,arriv®¶e,w,y);
 
-				int m=e.sommets[e.stringtoint(arriv√©e) + y].getdistance√†lasource();
+				int m=e.sommets[e.stringtoint(arriv®¶e) + y].getdistance®§lasource();
 
 
 
@@ -251,7 +281,7 @@ public class PlusCourtChemin {
 
 
 
-		g.calculpluscourtedistance(d√©part,arriv√©e,k,z);
+		g.calculpluscourtedistance(d®¶part,arriv®¶e,k,z);
 
 
 
@@ -265,15 +295,208 @@ public class PlusCourtChemin {
 
 		Collections.reverse(path);
 
+		
+		BufferedImage image = ImageIO.read(new File("./src/application/plan.png"));
+		Graphics2D graphics = image.createGraphics();
+		graphics.setStroke(new BasicStroke(3.0f));
+		for (int i = 0; i < (cord.length+1)/2; i++) {
+			Ellipse2D e = new Ellipse2D.Float(g.sommets[i].x,g.sommets[i].y,2.0f,2.0f);
+			switch(g.sommets[i].ligne){
+			case 1:
+			graphics.setColor(Color.black);
+			graphics.draw(e);
+			break;
+			case 2:
+			graphics.setColor(Color.blue);
+			graphics.draw(e);
+			break;
+			case 3:
+			graphics.setColor(Color.cyan);
+			graphics.draw(e);
+			break;
+			case 4:
+			graphics.setColor(Color.darkGray);
+			graphics.draw(e);
+			break;
+			case 5:
+			graphics.setColor(Color.gray);
+			graphics.draw(e);
+			break;
+			case 6:
+			graphics.setColor(Color.green);
+			graphics.draw(e);
+			break;
+			case 7:
+			graphics.setColor(Color.lightGray);
+			graphics.draw(e);
+			break;
+			case 8:
+			graphics.setColor(Color.magenta);
+			graphics.draw(e);
+			break;
+			case 9:
+			graphics.setColor(Color.orange);
+			graphics.draw(e);
+			break;
+			case 10:
+			graphics.setColor(Color.pink);
+			graphics.draw(e);
+			break;
+			case 11:
+			graphics.setColor(Color.red);
+			graphics.draw(e);
+			break;
+			case 12:
+			graphics.setColor(Color.yellow);
+			graphics.draw(e);
+			break;
+			case 13:
+			graphics.setColor(Color.blue);
+			graphics.draw(e);
+			break;
+			case 14:
+			graphics.setColor(Color.cyan);
+			graphics.draw(e);
+			break;
+			case 15:
+			graphics.setColor(Color.green);
+			graphics.draw(e);
+			break;
+			case 16:
+			graphics.setColor(Color.lightGray);
+			graphics.draw(e);
+			break;
+			}
+		}	
+
+		switch(g.sommets[g.stringtoint(d®¶part)].ligne){
+		case 1:
+		graphics.setColor(Color.black);
+		break;
+		case 2:
+		graphics.setColor(Color.blue);
+		break;
+		case 3:
+		graphics.setColor(Color.cyan);;
+		break;
+		case 4:
+		graphics.setColor(Color.darkGray);
+		break;
+		case 5:
+		graphics.setColor(Color.gray);
+		break;
+		case 6:
+		graphics.setColor(Color.green);
+		break;
+		case 7:
+		graphics.setColor(Color.lightGray);
+		break;
+		case 8:
+		graphics.setColor(Color.magenta);
+		break;
+		case 9:
+		graphics.setColor(Color.orange);
+		break;
+		case 10:
+		graphics.setColor(Color.pink);
+		break;
+		case 11:
+		graphics.setColor(Color.red);
+		break;
+		case 12:
+		graphics.setColor(Color.yellow);
+		break;
+		case 13:
+		graphics.setColor(Color.blue);
+		break;
+		case 14:
+		graphics.setColor(Color.cyan);
+		break;
+		case 15:
+		graphics.setColor(Color.green);
+		break;
+		case 16:
+		graphics.setColor(Color.lightGray);
+		break;
+		}
+		graphics.drawString(g.sommets[g.stringtoint(d®¶part)].nom, g.sommets[g.stringtoint(d®¶part)].x, g.sommets[g.stringtoint(d®¶part)].y);
+		graphics.draw(new Line2D.Double(g.sommets[g.stringtoint(d®¶part)].x,g.sommets[g.stringtoint(d®¶part)].y , path.get(0).x,path.get(0).y));
+		
+		for (int i = 0; i < path.size()-1; i++) {
+			switch(path.get(i).ligne){
+			case 1:
+			graphics.setColor(Color.black);
+			break;
+			case 2:
+			graphics.setColor(Color.blue);
+			break;
+			case 3:
+			graphics.setColor(Color.cyan);;
+			break;
+			case 4:
+			graphics.setColor(Color.darkGray);
+			break;
+			case 5:
+			graphics.setColor(Color.gray);
+			break;
+			case 6:
+			graphics.setColor(Color.green);
+			break;
+			case 7:
+			graphics.setColor(Color.lightGray);
+			break;
+			case 8:
+			graphics.setColor(Color.magenta);
+			break;
+			case 9:
+			graphics.setColor(Color.orange);
+			break;
+			case 10:
+			graphics.setColor(Color.pink);
+			break;
+			case 11:
+			graphics.setColor(Color.red);
+			break;
+			case 12:
+			graphics.setColor(Color.yellow);
+			break;
+			case 13:
+			graphics.setColor(Color.blue);
+			break;
+			case 14:
+			graphics.setColor(Color.cyan);
+			break;
+			case 15:
+			graphics.setColor(Color.green);
+			break;
+			case 16:
+			graphics.setColor(Color.lightGray);
+			break;
+			}
+			graphics.draw(new Line2D.Double(path.get(i).x,path.get(i).y,path.get(i+1).x,path.get(i+1).y));
+			
+			graphics.drawString(path.get(i).nom, path.get(i).x, path.get(i).y);
+		}
+		
+		
+		graphics.draw(new Line2D.Double(path.get(path.size()-1).x,path.get(path.size()-1).y,g.sommets[g.stringtoint(arriv®¶e)].x ,g.sommets[g.stringtoint(arriv®¶e)].y));
+		
+		graphics.drawString(g.sommets[g.stringtoint(arriv®¶e)].nom, g.sommets[g.stringtoint(arriv®¶e)].x, g.sommets[g.stringtoint(arriv®¶e)].y);
+		
+		graphics.dispose();
+		
+		if (this.b){
+		ImageIO.write(image, "png", new File("./bin/application/abc.png"));
+		}
+		else{
+		ImageIO.write(image, "png", new File("./bin/application/abc2.png"));	
+		}
 
 
 
+		if (h.contains(arriv®¶e)) {
 
-
-
-		if (h.contains(arriv√©e)) {
-
-			System.out.println(g.sommets[g.stringtoint(d√©part)].ligne);
+			System.out.println(g.sommets[g.stringtoint(d®¶part)].ligne);
             if (g.chemin.get(0+k).ligne==15) {
 			h.set(0, h.get(0)+"(" + "3bis"+")");
             }
@@ -295,7 +518,7 @@ public class PlusCourtChemin {
 
 			}
 
-			if ((g.sommets[g.stringtoint(arriv√©e)+z].getdistance√†lasource()-25)>3600000) {
+			if ((g.sommets[g.stringtoint(arriv®¶e)+z].getdistance®§lasource()-25)>3600000) {
 
 				texte1="";
 
@@ -312,10 +535,10 @@ public class PlusCourtChemin {
 
 
             if (this.b) {
-			texte2=g.afficherr√©sultat(d√©part, arriv√©e,z,1);
+			texte2=g.afficherr®¶sultat(d®¶part, arriv®¶e,z,1);
             }
             else {
-            	texte2=g.afficherr√©sultat(d√©part, arriv√©e,z,2);
+            	texte2=g.afficherr®¶sultat(d®¶part, arriv®¶e,z,2);
             }
 
 
@@ -323,7 +546,7 @@ public class PlusCourtChemin {
 
 		else {
 
-			h.add(arriv√©e);
+			h.add(arriv®¶e);
 
 
 
@@ -357,7 +580,7 @@ public class PlusCourtChemin {
 
 				}
 			}
-			if ((g.sommets[g.stringtoint(arriv√©e)+z].getdistance√†lasource()-25)>3600000) {
+			if ((g.sommets[g.stringtoint(arriv®¶e)+z].getdistance®§lasource()-25)>3600000) {
 
 				texte1="";
 
@@ -376,10 +599,10 @@ public class PlusCourtChemin {
 
 
 			 if (this.b) {
-					texte2=g.afficherr√©sultat(d√©part, arriv√©e,z,1);
+					texte2=g.afficherr®¶sultat(d®¶part, arriv®¶e,z,1);
 		            }
 		            else {
-		            	texte2=g.afficherr√©sultat(d√©part, arriv√©e,z,2);
+		            	texte2=g.afficherr®¶sultat(d®¶part, arriv®¶e,z,2);
 		            }
 
 
